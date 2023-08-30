3ad687f2049dd197177128c7d713f9b88b8863be
Bowen(Brad) Xu <7578594+maxxbw54@users.noreply.github.com>
2023-08-29 21:07:10 -0400

    clean code

--- exercise/play-with-chatgpt-via-api/play-chatgpt.py
+++ exercise/play-with-chatgpt-via-api/play-chatgpt.py
@@ -8,8 +8,6 @@ with open('api.key', 'r') as file:
 # Set your OpenAI API key
 openai.api_key = content
 
-defect_code = "static void filter_mirror_setup(NetFilterState *nf, Error **errp)\n{\n    MirrorState *s = FILTER_MIRROR(nf);\n    Chardev *chr;\n    chr = qemu_chr_find(s->outdev);\n    if (chr == NULL) {\n        error_set(errp, ERROR_CLASS_DEVICE_NOT_FOUND,\n                  \"Device '%s' not found\", s->outdev);\n    qemu_chr_fe_init(&s->chr_out, chr, errp);"
-
 conversation_history = [{"role": "system", "content": "You are a weather reporter."}]
 
 while True:
